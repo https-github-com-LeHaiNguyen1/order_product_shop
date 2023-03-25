30c23875c907c44adf7ed36a3cb3308e0d4f8640
nguyen_le_hai <nguyen_le_hai@management-partners.co.jp>
2023-03-25 23:46:36 +0700

    Update debugbar - on commit google and fix error jquery-easing

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -36,7 +36,7 @@
     <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset('adminvendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,13 +51,13 @@
                                 <button class="btn btn-primary btn-user btn-block">
                                     Register
                                 </button>
-                                {{-- <hr>
+                                <hr>
                                 <a href="index.html" class="btn btn-google btn-user btn-block">
                                     <i class="fab fa-google fa-fw"></i> Login with Google
                                 </a>
                                 <a href="index.html" class="btn btn-facebook btn-user btn-block">
                                     <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a> --}}
+                                </a>
                             </form>
                             <hr>
                             <div class="text-center">
