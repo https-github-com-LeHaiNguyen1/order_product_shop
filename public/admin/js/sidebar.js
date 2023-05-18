3179c7387c00e6fcd50d4f1426ff44c6a0003c24
nguyen_le_hai <nguyen_le_hai@management-partners.co.jp>
2023-05-18 13:51:08 +0700

    delete

--- public/admin/js/sidebar.js
+++
@@ -1,7 +0,0 @@
-$("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
-    };
-  });
